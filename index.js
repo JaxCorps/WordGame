225ca6fb49fc60e50415d7007210c21d89a4316d
ThatGravyboat <gravyboat211@gmail.com>
2020-08-16 20:37:43 -0230

    Added daily stats

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var revealedWord = "";
 var define = document.querySelector( "#define" );
 var answer = document.querySelector( "#answer" );
 var dailySolvedCount = 0;
+var players = new Map;
 
 function addpoints(name, points) {
 
@@ -64,6 +65,16 @@ function checkWord( selectedWord, typedword ) {
   return !revealedWord.includes( "_" );
 }
 
+function addWordGuessedCount(user) {
+  if (players.get(user) === 0 || !players.has(user)){
+    players.set(user, 1)
+  } else {
+    var points = players.get(user) + 1;
+    players.set(user, points)
+  }
+}
+
+
 ComfyJS.onChat = ( user, message,flags, self, extra ) => {
   if( checkWord( selectedWord.toLowerCase(), message.toLowerCase().split( " " )[ 0 ] ) ) {
     if(!isSolved) {
@@ -73,6 +84,7 @@ ComfyJS.onChat = ( user, message,flags, self, extra ) => {
       document.querySelector( ".recentWinner" ).innerText = "Last Winner: " + user;
       addpoints(user, selectedWord.length * 75);
       ComfyJS.Say(`${user} has solved the word!`);
+      addWordGuessedCount(user.toLowerCase());
       setTimeout( () => {
         newWord();
       }, 3000 );
@@ -89,10 +101,23 @@ ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
   if(command === "solvedcount" ) {
     ComfyJS.Say( `@${user} todays daily solved word count is ${dailySolvedCount}.` );
   }
+  if(command === "wordcount" ) {
+    if (message.trim().toLowerCase().length > 0){
+      if (players.has(message.trim().toLowerCase().replace("@", ""))){
+        ComfyJS.Say( `@${user}, ${message.trim()}'s amount of words guessed correctly today is ${players.get(message.trim().toLowerCase().replace("@", ""))}.` );
+      } else {
+        ComfyJS.Say( `@${user} please enter a valid username or dont add a message at the end to see your own stats.` );
+      }
+    }else {
+      ComfyJS.Say( `@${user} your amount of words guessed correctly today is ${players.get(user.toLowerCase())}.` );
+    }
+  }
+
   if( flags.broadcaster && command === "newword" ) {
     ComfyJS.Say( `/me The word was ${selectedWord}.` );
     newWord();
   }
+
   if (extra.customRewardId == "f18a863c-146e-4f0f-9357-cd0456c136a4") {
     ComfyJS.Say( `/me ${user} has skipped the word!` );
     ComfyJS.Say( `/me the word skipped was ${selectedWord}.`);
